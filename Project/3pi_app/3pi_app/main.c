22021af884d63fce0c26c2ee84ca53a1e24bf05c
niksanaznc <progressiven6@gmail.com>
2019-04-02 23:02:32 +0300

    Update main.c
    Reformatting the code and adding some comments.

--- Project/3pi_app/3pi_app/main.c
+++ Project/3pi_app/3pi_app/main.c
@@ -1,22 +1,51 @@
 // The 3pi include file must be at the beginning of any program that
 // uses the Pololu AVR library and 3pi.
 #include <pololu/3pi.h>
-
 #include <avr/pgmspace.h>
 
+// This is the main function, where the code starts.  All C programs
+// must have a main() function defined somewhere.
+int main()
+{
+	// set up the 3pi
+	initialize();
+	
+	test_sensors();
+	//mainRobotLogic();
+}
+
+// Waits for Button B to be pressed for user convenience and then
+// Initializes the line reading sensors
 void initialize()
 {
-	delay_ms(2000);
+	// Display battery voltage and wait for button press
+	while ( !button_is_pressed( BUTTON_B ) )
+	{
+		int bat = read_battery_millivolts();
+		
+		clear();
+		print_long( bat );
+		print( "mV" );
+		lcd_goto_xy( 0, 1 );
+		print( "Press B" );
+		
+		delay_ms( 100 );
+	}
 	
-	pololu_3pi_init(2000);
-	set_motors(0,0);
+	// Always wait for the button to be released so that 3pi doesn't
+	// start moving until your hand is away from it.
+	wait_for_button_release( BUTTON_B );
+	delay_ms( 1000 );
+	
+	pololu_3pi_init( 2000 );
+	set_motors( 0, 0 );
 }
 
 void turn_left()
 {
-	set_motors(-42, 42);
-	delay_ms(400);
-	set_motors(0,0);
+	set_motors( -42, 42 );
+	delay_ms( 400 );
+	set_motors( 0, 0 );
 }
 
 void go_forward()
@@ -25,69 +54,61 @@ void go_forward()
 
 void turn_right()
 {
-	set_motors(41, -41);
-	delay_ms(400);
-	set_motors(0,0);
+	set_motors( 41, -41 );
+	delay_ms( 400 );
+	set_motors( 0, 0 );
 }
 
 void turn_back()
 {
-	set_motors(41, -41);
-	delay_ms(800);
-	set_motors(0,0);
+	set_motors( 41, -41 );
+	delay_ms( 800 );
+	set_motors( 0, 0 );
 }
 
-// This is the main function, where the code starts.  All C programs
-// must have a main() function defined somewhere.
-int main()
+// Tests data returned from the sensors
+void test_sensors()
 {
+	unsigned int	sensors[ 5 ]; // an array to hold sensor values
 	
-	unsigned int sensors[5]; // an array to hold sensor values
-	int moving = 1;
-	int iter = 0;
-	
-	// set up the 3pi
-	initialize();
-	
-	test_sensors(sensors);
-	//mainRobotLogic(sensors);
-}
-
-void test_sensors(unsigned int sensors[5]){
-	int moving = 1;
-	int iter = 0;
-	while(1)
+	while ( 1 )
 	{
 		read_line_sensors( sensors, IR_EMITTERS_ON );
-		for(int i =0;i<5;i++)
+		
+		// Display the sensor results
+		for( int i = 0; i < 5; i++ )
 		{
-			print_long(sensors[i]);
-			delay_ms(500);
+			print_long( sensors[ i ] );
+			delay_ms( 500 );
 			clear();
 		}
-		play(">>a32");
+		
+		// Play sound to notify that the next iteration will begin.
+		play( ">>a32" );
 	}
-
 }
 
-
-void mainRobotLogic(unsigned int sensors[5])
+// Our main logic function
+void mainRobotLogic()
 {
-	int moving = 1;
-	int iter = 0;
-	while(1)
+	unsigned int	sensors[ 5 ]; // an array to hold sensor values
+	int				moving	= 1; // This flag tells us if we have to look for the marker color. If it is 1, we are on a marker and have to decide where to go.
+	int				iter	= 0; // Should be replaced with posX and posY
+	
+	while ( 1 )
 	{
 		read_line_sensors( sensors, IR_EMITTERS_ON );
-		//test_sensors(sensors);
-		if ( sensors[1] > 800 || sensors[2] > 800 || sensors[3] > 800 )
+
+		if ( sensors[1] > 800 || sensors[2] > 800 || sensors[3] > 800 ) // is_on_marker(sensors)
 		{
 			if ( moving )
 			{
+				// Decide where to go next
 				iter++;
-				play(">>a32");
+				play( ">>a32" );
 				moving = 0;
 				
-				delay_ms(300);
+				delay_ms( 300 );
 				set_motors( 0, 0 );
 				
 				if ( iter == 4 )
@@ -105,10 +126,10 @@ void mainRobotLogic(unsigned int sensors[5])
 			moving = 1;
 		}
 		
-		if(iter<7){
+		if ( iter < 7 )
+		{
 			set_motors( 20, 20 );
 		}
-		
 	}
 }
 //Ctrl+K+D
